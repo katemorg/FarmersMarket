75dd306f6a355a3abd287cd8eb6e7ea61bbdbd7f
arabgangsta690 <vr4turbo@gmail.com>
2017-06-15 18:55:36 -0400

    Add files via upload

---
+++ assets/javascript/main.js
@@ -0,0 +1,136 @@
+// // Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyCMqAsnjjRovdQtvPkQrpMV1T8hMcPfrZo",
+//   authDomain: "farmers-market-1497106306900.firebaseapp.com",
+//   databaseURL: "https://farmers-market-1497106306900.firebaseio.com",
+//   projectId: "farmers-market-1497106306900",
+//   storageBucket: "farmers-market-1497106306900.appspot.com",
+//   messagingSenderId: "453822887828"
+// };
+// firebase.initializeApp(config);
+var database = firebase.database();
+var markets = [];
+
+function farmersMarket() {
+  this.id = null;
+  this.name = null;
+  this.address = null;
+  this.googleLink = null;
+  this.products = null;
+  this.schedule = null;
+}
+
+// Search function
+$(function() {
+  $("#searchZip").on("click", function() {
+    var zipcode = $("#textZip").val().trim();
+    getMarkets(zipcode);
+  });
+
+});
+
+
+function getMarkets(zip) {
+  $.ajax({
+    type: "GET",
+    contentType: "application/json; charset=utf-8",
+    // submit a get request to the restful service mktDetail.
+    // url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
+    url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
+    dataType: 'jsonp',
+    jsonpCallback: 'marketResultHandler'
+  });
+}
+
+//iterate through the JSON result object.
+function marketResultHandler(detailresults) {
+  var marketList = detailresults.results;
+  for (var key in marketList) {
+    var newMarket = new farmersMarket();
+    newMarket.id = marketList[key].id;
+    newMarket.name = marketList[key].marketname;
+    markets.push(newMarket);
+  }
+
+  $.each(markets, function(index, value) {
+    getDetails(markets[index], index);
+  });
+}
+
+function getDetails(market, index) {
+  var id = market.id;
+  var name = market.name;
+
+  window['detailResultHandler' + id] = function(data) {
+    var currentMarket = data.marketdetails;
+    var newRow = ' \
+    <tr> \
+      <td>' + name + '</td> \
+      <td>' + '<a href="' + currentMarket["GoogleLink"] + '">' + currentMarket["Address"] + '</a></td> \
+      <td>' + currentMarket["Products"] + '</td> \
+      <td></td> \
+      <td>' + currentMarket["Schedule"] + '</td> \
+    </tr>';
+    $(".table tbody").append(newRow);
+  }
+
+  $.ajax({
+    type: "GET",
+    contentType: "application/json; charset=utf-8",
+    // submit a get request to the restful service mktDetail.
+    url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
+    // url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
+    dataType: 'jsonp',
+    jsonpCallback: 'detailResultHandler' + id
+  });
+}
+
+function displayMarkets(detailresults) {
+  var currentMarket = detailresults.marketdetails;
+  var newRow = ' \
+    <tr> \
+      <td>' + 'NAME GOES HERE' + '</td> \
+      <td>' + '<a href="' + currentMarket["GoogleLink"] + '">' + currentMarket["Address"] + '</a></td> \
+      <td>' + currentMarket["Products"] + '</td> \
+      <td></td> \
+      <td>' + currentMarket["Schedule"] + '</td> \
+    </tr>';
+  console.log(newRow);
+  $(".table tbody").append(newRow);
+}
+
+// Populate the table with the list farmers markets
+database.ref().on("value", function(snapshot) {
+    var data = snapshot.val();
+    $(".table tbody").empty();
+    if (data) {
+      for (var key in data) {
+        var thisObject = data[key];
+        console.log(data[key]);
+
+        // Add new row here
+      }
+    } else {
+      $(".table tbody").append("No farmers markets add one.")
+    }
+  },
+  function(errorObject) {
+    console.log("The read failed: " + errorObject.code)
+    $(".table tbody").append("Error getting farmers markets schedule!");
+  }
+);
+
+$("#button-submit").on("click", function() {
+
+});
+
+function addMarket() {
+  database.ref().push({
+    id: market.id,
+    name: market.name,
+    address: market.address,
+    googleLink: market.googleLink,
+    products: market.products,
+    schedule: market.schedule,
+  });
+}
---
+++ assets/javascript/search.js
@@ -0,0 +1,79 @@
+		var Market = "";
+        var Location = "";
+        var Products = "";
+        var Contact = "";
+        var Dates_Times = "";
+
+
+
+var displayTime = function () {
+    
+         database.ref().on("child_added", function (childSnapshot) {
+       
+        
+        var tableRow = $('<tr>').attr('id', todoCount).attr('data-key', childKey);
+        var name = $('<td>').text(childSnapshot.val().name);
+        var Location = $('<td>').text(childSnapshot.val().Location);
+        var Products = $('<td>').text(childSnapshot.val().Products);
+        var Contact = $('<td>').text(childSnapshot.val().Contact);
+        var Market = childSnapshot.val();
+        Market.id = childSnapshot.key;
+       
+        todoCount++;
+      
+        tableRow.append(nameCell).append(LocationCell).append(ProductsCell).append(ContactCell).append(MarketCell);
+        
+        $('tbody').append(tableRow);
+    });
+};
+
+    jsonObj = [];
+'child_added' function(column){
+    jsonObj.push(column);
+}
+
+
+$('#button-submit').on('click', function () {
+			name = $('#name').val().trim();
+			Location = $('#Location').val().trim();
+			Products = $('#Products').val().trim();
+			Contact = $('#Contact').val().trim();
+			
+			database.ref().push({
+                name: name,
+                Location: Location,
+                Products: Products,
+                Contact: Contact,
+
+
+             });
+
+			$('#name').val('');
+			$('#Location').val('');
+			$('#Products').val('');
+			$('#Contact').val('');
+			return false;
+    
+
+        
+
+		
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(function() {
+	});
