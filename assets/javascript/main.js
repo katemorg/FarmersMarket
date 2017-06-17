5b18f68bdfa4eabfae1f66daeb47ea0270a9cc3a
Dan Meyer <somesing9@gmail.com>
2017-06-17 11:51:16 -0400

    Chagned all request to https. Changed the modal--moreInfo so that it would populate on click with information for the market Changed map.js so that when the user allows their location it will get the users zipcode

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -38,7 +38,7 @@ function getMarkets(zip) {
   $.ajax({
     type: "GET",
     contentType: "application/json; charset=utf-8",
-    url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
+    url: "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
     dataType: 'jsonp',
     jsonpCallback: 'marketResultHandler'
   });
@@ -66,7 +66,7 @@ function getDetails(market, index) {
   window['detailResultHandler' + id] = function(data) {
     var currentMarket = data.marketdetails;
     var newRow = ' \
-    <tr> \
+    <tr data-name="' + name + '" data-address="' + currentMarket['Address'] + '" data-schedule="' + currentMarket['Schedule'] + '" data-products="' + currentMarket['Products'] + '""> \
       <td>' + name + '</td> \
       <td>' + '<a href="' + currentMarket["GoogleLink"] + '">' + currentMarket["Address"] + '</a></td> \
       <td>' + currentMarket["Schedule"] + '</td> \
@@ -78,7 +78,7 @@ function getDetails(market, index) {
   $.ajax({
     type: "GET",
     contentType: "application/json; charset=utf-8",
-    url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
+    url: "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
     dataType: 'jsonp',
     jsonpCallback: 'detailResultHandler' + id
   });
@@ -93,7 +93,7 @@ function displayMarkets(detailresults) {
       <td>' + currentMarket["Schedule"] + '</td> \
       <td>' + '<button type="button" class="btn btn-default btn-moreInfo" role="button" data-toggle="modal" data-target="#modal--moreInfo"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span></button>' + '</td> \
     </tr>';
-    $(".table tbody").append(newRow);
+  $(".table tbody").append(newRow);
 }
 
 // Populate the table with the list farmers markets
@@ -149,6 +149,7 @@ function addMarket() {
   });
 }
 
+
 // Populate modal fields
 $("#btn-AddMarket").on("click", function() {
   event.preventDefault();
@@ -166,3 +167,13 @@ $("#btn-AddMarket").on("click", function() {
   // });
 });
 
+
+$(".table").on("click", ".btn-moreInfo", function() {
+  event.preventDefault();
+  var marketName = $(this).closest("tr").attr("data-name");
+  var address = $(this).closest("tr").attr("data-address");
+  var products = $(this).closest("tr").attr("data-products");
+
+  $(".moreInfo-name").html(marketName);
+  $(".moreInfo-products").html(products);
+});
